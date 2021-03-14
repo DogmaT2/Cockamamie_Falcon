109058201dd28f7a0443d25af02a32b18e99a4d9
MikeFuller <mmfuller@protonmail.com>
2021-03-13 20:00:03 -0700

    skeleton files for main and functions

---
+++ main.R
@@ -0,0 +1,13 @@
+#####################################################
+# R CODE FILE
+# main.R
+# copyright Michael M Fuller
+# All rights reserved.
+#
+# Initation Date: March 13, 2021
+#
+# R script to govern process
+#
+
+
+
